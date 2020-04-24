66e477f7383a04bb55d0c9b7d09e2eef3b62bc3d
Vividh Mariy <vmariya_be19@thapar.edu>
2020-04-24 06:14:17 +0530

    Intial Commit, working with image + data body

---
+++ server.py
@@ -0,0 +1,24 @@
+from pyfcm import FCMNotification
+
+
+
+def uploadNotification(video_id, video_title, course):
+    key = "AAAAhHPOtR4:......"
+
+    push_service = FCMNotification(api_key=key)
+
+    data = {
+
+        "type":"upload",
+        "video_title":video_title,
+        "video_url":video_id,
+        "click_action":"FLUTTER_NOTIFICATION_CLICK",
+        "route":"youtube"
+    }
+    result = push_service.notify_topic_subscribers(topic_name=course, message_title="A new video has been uploaded", message_body="Please tap on this notification to continue", data_message=data, message_icon=f"https://img.youtube.com/vi/{data['video_url']}/hqdefault.jpg")
+
+    if result['success'] == 1:
+        print("notification sent") 
+
+if __name__ == "__main__":
+    uploadNotification("LLxdzKgNYcQ","FireFlies : Owl City", "allusers")
